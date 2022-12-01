da7307fe93af7f934805c9fa21a26e55c5207629
Chellyyyy <20520428@gm.uit.edu.vn>
2022-12-01 15:02:39 +0700

    width bị lỗi

---
+++ .history/build/scripts/firebase_20221130223930.js
@@ -0,0 +1,27 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD_g23L-wg8ve1sVjkFYV106u0Z6rs8MHY",
+    authDomain: "ie104-760b6.firebaseapp.com",
+    databaseURL: "https://ie104-760b6-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ie104-760b6",
+    storageBucket: "ie104-760b6.appspot.com",
+    messagingSenderId: "954364631488",
+    appId: "1:954364631488:web:c08eb53cbb2ae44aafd025"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  import {getDatabase, ref, set, child, update, remove} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+
+  const db = getDatabase();
+
+  var namebox = document.getElementById("NameBox");
+  var rollbox = document.getElementById("NameBox");
+  var namebx = document.getElementById("NameBox");
+  var namebox = document.getElementById("NameBox");
---
+++ .history/build/scripts/firebase_20221130230347.js
@@ -0,0 +1,49 @@
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD_g23L-wg8ve1sVjkFYV106u0Z6rs8MHY",
+    authDomain: "ie104-760b6.firebaseapp.com",
+    databaseURL: "https://ie104-760b6-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ie104-760b6",
+    storageBucket: "ie104-760b6.appspot.com",
+    messagingSenderId: "954364631488",
+    appId: "1:954364631488:web:c08eb53cbb2ae44aafd025"
+};
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+import {getDatabase, ref, set, g child, update, remove} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+
+const db = getDatabase();
+// ref
+var namebox = document.getElementById("Namebox");
+var rollbox = document.getElementById("Namebox");
+var secbox = document.getElementById("Namebox");
+var genbox = document.getElementById("Namebox");
+
+var insBtn = document.getElementById("Insbtn");
+var selBtn = document.getElementById("Selbtn");
+var updBtn = document.getElementById("Updbtn");
+var delBtn = document.getElementById("Delbtn");
+// insert data
+function InsertData(){
+    set(ref(db, "TheStudents/"+ rollbox.value),{
+        NameOfStd: namebox.value,
+        RollNo: rollbox.value,
+        Section: secbox.value,
+        Gender: genbox.value
+    })
+    .then(()=>{
+        alert("success");
+    })
+    .catch((error)=>{
+        alert("unsuccessful"+error);
+    })
+}
+// gán event vao nut
+insBtn.addEventListener('click', InsertData);
